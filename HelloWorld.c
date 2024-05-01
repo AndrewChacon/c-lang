f22f89f3cd8eefc7ae4195ed93d058ea679bd655
AndrewChacon <andrew.b.chacon@gmail.com>
2024-05-01 15:05:11 -0400

    basic c-lang program and learning how to compile and run c files

---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // add packages 
+
+int main() { // read procedures
+
+    printf("I like pizza!\n");
+    printf("Its really good");
+    return 0; // program exit status - 0 = no errors
+}
+
+// gcc HelloWorld.c   - compile program
+// ./a.out            - run program
+
+// COMMENTS AND ESCAPE SEQUENCES
